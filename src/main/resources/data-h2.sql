6869dd945239c95cc7b487c7bd6d1b767e9671d5
gigittelp <brigitte.lepevedicr@gmail.com>
2022-12-01 16:46:15 +0100

    musique spring boot

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,15 @@
+use MUSIQUE_DB;
+insert into artist(name, bio, fan_number)
+    values('Celtic woman','En 2004, les producteurs Sharon Browne et David Downes, directeur musical...',31760);
+insert into artist(name, bio, fan_number)
+    values('M. Pokora','Né le 26 septembre 1985 à Strasbourg d''un père footballeur et d''une mère fonctionnaire',1310570);
+insert into artist(name, bio, fan_number)
+    values('Kendji Girac','Kendji Maillé est né à Périgueux, en Dordogne, le 3 juillet 1996.',1014955);
+insert into artist(name, bio, fan_number)
+    values('Julien Doré','Julien Doré est né le 7 juillet 1982 à Alès dans le Gard. Après des études ',708365);
+insert into artist(name, bio, fan_number)
+    values('Patrick Fiori','Patrick Fiori, né le 23 septembre 1969 sous le nom de Jean-François Chouchayan',216351);
+insert into artist(name, bio, fan_number)
+    values('Trois Cafés Gourmands','',167304);
+insert into artist(name, bio, fan_number)
+    values ('Nolwenn Leroy','C''est le 28 septembre 1982 que voit le jour, à Saint-Renan (Finistère) que voile jour Nolwenn Le Magueresse',230900);
